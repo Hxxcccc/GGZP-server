b447e68116d05a574433f8d09aad1bee625b9cef
Hxc <hxc19980624@163.com>
2018-11-03 14:38:40 +0800

    添加约束对象

--- models/users.js
+++ models/users.js
@@ -16,7 +16,12 @@ const userSchema = new Schema({
   type: {
     type: String,
     required: true
-  }
+  },
+  header: String,
+  post: String,
+  info: String,
+  salary: String,
+  company: String
 });
 
 //创建模型对象
